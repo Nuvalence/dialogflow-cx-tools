8648e48b2cd3d67c4da75940204bfed6b3bf414d
Ginoel Teng <gteng@nuvalence.io>
2023-07-27 17:07:53 -0400

    UICCAI-550: created sheet copier

---
+++ cx-shared/src/main/kotlin/io/nuvalence/cx/tools/shared/SheetCopier.kt
@@ -0,0 +1,60 @@
+package io.nuvalence.cx.tools.shared
+
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
+import com.google.api.client.json.gson.GsonFactory
+import com.google.api.services.sheets.v4.Sheets
+import com.google.api.services.sheets.v4.model.*
+import java.net.URL
+
+/**
+ * Copy operations for Google Sheets.
+ *
+ * @param credentialsURL URL pointing to where the credentials.json file is (e.g.
+ * file:///path/to/credentials.json)
+ * @param spreadsheetId the ID of the Google Sheet to copy (See SheetReader for details
+ * about this parameter)
+ */
+class SheetCopier(credentialsURL: URL, private val spreadsheetId: String) {
+    private val transport = GoogleNetHttpTransport.newTrustedTransport()
+    private val service = Sheets.Builder(
+        transport,
+        GsonFactory.getDefaultInstance(),
+        Authorizer(credentialsURL, transport).getCredentials()
+    )
+        .setApplicationName("Dialogflow Agent Generator")
+        .build()
+
+    /**
+     * Copies a spreadsheet to a destination
+     *
+     * @param destinationTitle the title of the destination spreadsheet
+     *
+     * @return the spreadsheet ID of the newly generated spreadsheet
+     */
+    fun copySpreadsheet(destinationTitle: String): String {
+        // Create a new spreadsheet
+        val newSpreadsheet = Spreadsheet()
+        val createdSpreadsheet = service.spreadsheets().create(newSpreadsheet).execute()
+        val destinationSpreadsheetId = createdSpreadsheet.spreadsheetId
+
+        // Rename new spreadsheet to specified title
+        val renameRequest = BatchUpdateSpreadsheetRequest().setRequests(
+            listOf(Request().setUpdateSpreadsheetProperties(
+                UpdateSpreadsheetPropertiesRequest().setProperties(
+                    SpreadsheetProperties().setTitle(destinationTitle)
+                ).setFields("title")
+            ))
+        )
+        service.spreadsheets().batchUpdate(destinationSpreadsheetId, renameRequest).execute()
+
+        // Copy the all sheets from the source spreadsheet to the destination spreadsheet
+        val request = CopySheetToAnotherSpreadsheetRequest().setDestinationSpreadsheetId(destinationSpreadsheetId)
+        service.spreadsheets().get(spreadsheetId).execute()
+            .sheets.forEachIndexed { index, _ ->
+                service.spreadsheets().sheets().copyTo(spreadsheetId, index, request).execute()
+            }
+
+        return destinationSpreadsheetId
+    }
+
+}
