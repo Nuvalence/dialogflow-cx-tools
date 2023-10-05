2f4ebce28473ebfbfb0b1798a9586da63367e569
Ginoel Teng <gteng@nuvalence.io>
2023-10-05 16:50:07 -0400

    UICCAI-891: cleanup

--- cx-test/src/test/kotlin/io/nuvalence/cx/tools/cxtest/TestBuilderSpec.kt
+++ cx-test/src/test/kotlin/io/nuvalence/cx/tools/cxtest/TestBuilderSpec.kt
@@ -11,52 +11,52 @@ import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
-fun parseJson(data: List<ConversationTurn>?) {
-    for (i in data?.indices!!) {
-        val turn = data[i];
-
-        // User Input
-        if (turn.hasUserInput()) {
-            val userInput = turn.userInput
-            if (userInput.hasInput()) {
-                val input = userInput.input
-                if (input.hasText()) {
-                    val text = input.text
-                    println("CALLER SAYS: ${text.text}")
-                }
-            }
-        }
-
-        // Virtual Agent Output
-        if (turn.hasVirtualAgentOutput()) {
-            val virtualAgentOutput = turn.virtualAgentOutput
-            if (virtualAgentOutput.textResponsesCount > 0) {
-                val textResponses = virtualAgentOutput.textResponsesList
-                for (j in 0 until textResponses.size) {
-                    val response = textResponses[j]
-                    if (response.textCount > 0) {
-                        println("AGENT SAYS: ${response.textList}")
-                    }
-                }
-            }
-            if (virtualAgentOutput.differencesCount > 0) {
-                val differences = virtualAgentOutput.differencesList
-
-                println(differences.toString())
-            }
-        }
-    }
-}
-
 @Tag("test")
 @ExtendWith(TestBuilderTestExtension::class)
 class TestBuilderSpec {
+    fun parseJson(data: List<ConversationTurn>?) {
+        for (i in data?.indices!!) {
+            val turn = data[i];
+
+            // User Input
+            if (turn.hasUserInput()) {
+                val userInput = turn.userInput
+                if (userInput.hasInput()) {
+                    val input = userInput.input
+                    if (input.hasText()) {
+                        val text = input.text
+                        println("CALLER SAYS: ${text.text}")
+                    }
+                }
+            }
+
+            // Virtual Agent Output
+            if (turn.hasVirtualAgentOutput()) {
+                val virtualAgentOutput = turn.virtualAgentOutput
+                if (virtualAgentOutput.textResponsesCount > 0) {
+                    val textResponses = virtualAgentOutput.textResponsesList
+                    for (j in 0 until textResponses.size) {
+                        val response = textResponses[j]
+                        if (response.textCount > 0) {
+                            println("AGENT SAYS: ${response.textList}")
+                        }
+                    }
+                }
+                if (virtualAgentOutput.differencesCount > 0) {
+                    val differences = virtualAgentOutput.differencesList
+
+                    println(differences.toString())
+                }
+            }
+        }
+    }
+
     @Test
     fun `should update ssn`() {
 
-        val testClient = TestBuilderTestExtension.testClient
+        val testClient = TestBuilderTestExtension.testClient!!
 
-        val testCase = testClient?.getTestCase(
+        val testCase = testClient.getTestCase(
             TestCaseName.of(
                 "dol-uisim-ccai-dev-app",
                 "global",
@@ -69,34 +69,25 @@ class TestBuilderSpec {
         println("Tags: ${testCase?.tagsList}")
         println("Notes: ${testCase?.notes}")
 
-        val ssnTurns : MutableList<Int> = mutableListOf()
-        testCase?.testCaseConversationTurnsList?.forEachIndexed { index, turn ->
+        val updatedTestCaseBuilder = testCase.toBuilder()!!
+        testCase.testCaseConversationTurnsList?.forEachIndexed { index, turn ->
             if (index > 0 && testCase.testCaseConversationTurnsList!![index - 1].virtualAgentOutput.sessionParameters.fieldsMap["data-collection-type"]?.stringValue == "ssn" &&
                 turn.userInput.input.text.text.matches(Regex(".*\\d{9}.*"))) {
-                ssnTurns.add(index)
-            }
-        }
-
-        val updatedTestCaseBuilder = testCase?.toBuilder()!!
-
-        ssnTurns.forEach { turnIndex ->
-            val originalStep = testCase.testCaseConversationTurnsList?.get(turnIndex)!!
-
-            val updatedStep = originalStep.toBuilder()
-                .setUserInput(originalStep.userInput?.toBuilder()!!.setInput(
-                    QueryInput.newBuilder().setText(
-                        TextInput.newBuilder().setText("987654321").build())
+                val updatedTurn = turn.toBuilder()
+                    .setUserInput(turn.userInput?.toBuilder()!!.setInput(
+                        QueryInput.newBuilder().setText(
+                            TextInput.newBuilder().setText("987654321").build())
                     ).build()
-                )
+                    )
                     /*
-                .setVirtualAgentOutput(originalStep.virtualAgentOutput?.toBuilder()!!.setSessionParameters(
-                    originalStep.virtualAgentOutput.sessionParameters.toBuilder()!!.fieldsMap["ssn"] = "987654321"
-                ))
+                    .setVirtualAgentOutput(originalStep.virtualAgentOutput?.toBuilder()!!.setSessionParameters(
+                        originalStep.virtualAgentOutput.sessionParameters.toBuilder()!!.fieldsMap["ssn"] = "987654321"
+                    ))
+                    */
+                    .build()
 
-                     */
-                .build()
-
-            updatedTestCaseBuilder.setTestCaseConversationTurns(turnIndex, updatedStep)
+                updatedTestCaseBuilder.setTestCaseConversationTurns(index, updatedTurn)
+            }
         }
 
         updatedTestCaseBuilder.clearTags()
@@ -106,8 +97,8 @@ class TestBuilderSpec {
             .setTestCase(updatedTestCaseBuilder.build())
             .setUpdateMask(FieldMask.newBuilder().addAllPaths(listOf("test_case_conversation_turns", "tags")).build())
             .build()
-        // test_case_conversation_turns.user_input.input.text.text
 
         testClient.updateTestCase(updateRequest)
     }
 }
+
