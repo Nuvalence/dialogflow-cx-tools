02db984ea6a6047de414d847350d0dd91a9b5b35
Nathanial Tranel <nathanialtranel@nuvalence-mb90.lan>
2023-10-27 14:18:53 -0700

    ignore numbers less than 4 characters in length for ssml processing

--- cx-phrases/src/main/kotlin/io/nuvalence/cx/tools/phrases/Ssml.kt
+++ cx-phrases/src/main/kotlin/io/nuvalence/cx/tools/phrases/Ssml.kt
@@ -179,7 +179,7 @@ fun processUrl(url: String) =
 
 fun processNumber(number: String) =
     // if a number has more than 3 digits, say one digit at a time, and make sure we say "zero", not "oh"
-    (if (number.length > 3)
+    (if (number.length > 4)
         START_PROSODY_RATE + START_SAY_VERBATIM + number + END_SAY_VERBATIM + END_PROSODY_RATE
     else
         number
