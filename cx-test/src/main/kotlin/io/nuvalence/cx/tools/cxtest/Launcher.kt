7b4ade1be9a81b9b65bd0a7f7239c12ae3cab848
Ginoel Teng <gteng@nuvalence.io>
2023-11-02 15:12:50 -0400

    UICCAI-864: added classpath and include/exclude tags support, updated readme

--- cx-test/src/main/kotlin/io/nuvalence/cx/tools/cxtest/Launcher.kt
+++ cx-test/src/main/kotlin/io/nuvalence/cx/tools/cxtest/Launcher.kt
@@ -8,28 +8,84 @@ import org.junit.platform.launcher.TagFilter
 import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder
 import org.junit.platform.launcher.core.LauncherFactory
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener
+import java.io.File
+import java.net.URL
+import java.util.*
+import java.util.jar.JarFile
 
 class Launcher {
     companion object {
+        fun findClassesInPackage(packageName: String): List<String> {
+            val classNames = mutableListOf<String>()
+            val path = packageName.replace('.', '/')
+            val resources: Enumeration<URL> = Thread.currentThread().contextClassLoader.getResources(path)
+
+            while (resources.hasMoreElements()) {
+                val resource = resources.nextElement()
+                if (resource.protocol.equals("jar", ignoreCase = true)) {
+                    val jarPath = resource.path.substring(5, resource.path.indexOf("!"))
+                    classNames.addAll(getClassNamesFromJar(jarPath, path))
+                } else if (resource.protocol.equals("file", ignoreCase = true)) {
+                    classNames.addAll(getClassNamesFromFile(resource.path, path))
+                }
+            }
+
+            return classNames
+        }
+
+        fun getClassNamesFromJar(jarPath: String, path: String): List<String> {
+            val classNames = mutableListOf<String>()
+
+            JarFile(jarPath).use { jar ->
+                val entries = jar.entries()
+                while (entries.hasMoreElements()) {
+                    val entry = entries.nextElement()
+                    if (entry.name.startsWith(path) && entry.name.endsWith(".class")) {
+                        val className = entry.name.replace("/", ".").replace(".class", "")
+                        classNames.add(className)
+                    }
+                }
+            }
+
+            return classNames
+        }
+
+        fun getClassNamesFromFile(buildDir: String, path: String): List<String> {
+            val baseDir = File(buildDir)
+            return baseDir.walkTopDown()
+                .filter { it.isFile && it.extension == "class" }
+                .map { it.relativeTo(baseDir).path }
+                .map { "$path.$it" }
+                .map { it.removeSuffix(".class").replace(File.separatorChar, '.') }
+                .toList()
+        }
+
         @JvmStatic
         fun main(args: Array<String>) {
             Properties.init(args[0])
 
-            val request: LauncherDiscoveryRequest = LauncherDiscoveryRequestBuilder.request()
+            val includeTags = Properties.INCLUDE_TAGS
+            val excludeTags = Properties.EXCLUDE_TAGS
+            val testPackage = Properties.TEST_PACKAGE
+            val classNames = findClassesInPackage(testPackage)
+
+            val requestBuilder: LauncherDiscoveryRequestBuilder = LauncherDiscoveryRequestBuilder.request()
                 .filters(EngineFilter.includeEngines("junit-jupiter"))
-                .selectors(selectClass(DFCXTestBuilderSpec::class.java))
                 .configurationParameter("junit.jupiter.execution.parallel.enabled", "true")
                 .configurationParameter("junit.jupiter.execution.parallel.config.strategy", "fixed")
                 .configurationParameter("junit.jupiter.execution.parallel.config.fixed.parallelism", "4")
-                .filters(TagFilter.includeTags("dfcx"), TagFilter.excludeTags("e2e|smoke"))
-                .build()
+                .filters(TagFilter.includeTags(includeTags), TagFilter.excludeTags(excludeTags))
+
+            classNames.forEach { className ->
+                requestBuilder.selectors(selectClass(className))
+            }
 
             val launcher = LauncherFactory.create()
 
             val summaryListener = SummaryGeneratingListener()
             launcher.registerTestExecutionListeners(summaryListener)
 
-            launcher.execute(request)
+            launcher.execute(requestBuilder.build())
 
             println("Tests found: ${summaryListener.summary.testsFoundCount}")
             println("Tests succeeded: ${summaryListener.summary.testsSucceededCount}")
